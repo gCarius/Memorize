bfc7e17a45b9221bd0b79bd98f3faa4595b9b593
Gui Carius <carius@ualberta>
2025-05-07 13:21:14 -0600

    lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return Text("Hello, world!")
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView(isFacedUp: true)
+            }
+            
+        }
+        .padding()
+        .foregroundColor(Color.brown)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFacedUp: Bool
+    
+    var body: some View {
+        ZStack() {
+            if isFacedUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 6)
+                Text("🥚")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.brown)
+            }
+            
+            
+        }
     }
 }
 
