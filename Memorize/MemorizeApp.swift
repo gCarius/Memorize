d906075101e62d28f436f685a36f4cdc3da684f8
Gui Carius <carius@ualberta>
2025-05-07 11:35:34 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Guilherme Carius on 2025-05-07.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
